e8e05512f65c7a89736be34131413fc2db13f5f4
broth59 <broth59@naver.com>
2020-08-11 15:26:18 +0900

    Update regular-revision

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,11 @@ const markdown = require('gulp-markdown')
 const scss = require('gulp-sass')
 const concat = require('gulp-concat')
 const replace = require('gulp-replace')
-const inlineFonts = require('gulp-inline-fonts');
-    //minify
-    const uglify  = require('gulp-uglify-es').default
-    const htmlmin = require('gulp-htmlmin')
-    const minify  = require('gulp-minify')
+const inlineFonts = require('gulp-inline-fonts')
+//minify
+const uglify = require('gulp-uglify-es').default
+const htmlmin = require('gulp-htmlmin')
+const minify = require('gulp-minify')
 
 const css_base64 = require('gulp-base64')
 const html_base64 = require('gulp-inline-image-html')
@@ -30,252 +30,252 @@ Path.dist = path.join(__dirname, 'dist')
 Path.temp = path.join(__dirname, 'temp')
 Path.bundle = path.join(__dirname, 'bundle')
 
-Path.js  = path.join(__dirname, 'src', 'static', 'js')
+Path.static = path.join(__dirname, 'src', 'static')
+Path.js = path.join(__dirname, 'src', 'static', 'js')
 Path.css = path.join(__dirname, 'src', 'static', 'css')
+Path.img = path.join(__dirname, 'src', 'static', 'img')
 Path.html = path.join(__dirname, 'src', 'static', 'html')
 
 //static files
-const code_syntax_js  = gulp.src(path.join(Path.js, 'highlight.pack.js'))
+const code_syntax_js = gulp.src(path.join(Path.js, 'highlight.pack.js'))
 const code_syntax_css = gulp.src(path.join(Path.css, 'styles', 'a11y-dark.css'))
 
 const markdown_default_css = toStyle(fs.readFileSync(path.join(Path.css, 'styles', 'splendor.css')))
 
-    //static html
-    const meta_tag_html = fs.readFileSync(path.join(Path.html, 'meta_tag.html'))
+//static html
+const meta_tag_html = fs.readFileSync(path.join(Path.html, 'meta_tag.html'))
 
-
-function toScript(js){
-    return `<script>${js}</script>`
+function toScript(js) {
+	return `<script>${js}</script>`
 }
-function toStyle(css){
-    return `<style>${css}</style>`
+function toStyle(css) {
+	return `<style>${css}</style>`
 }
 
-function wrapContents(contents_buffer){
-    return Buffer.concat([
-        Buffer.from(`
+function wrapContents(contents_buffer) {
+	return Buffer.concat([
+		Buffer.from(`
         <section id='background'></section>
         <ul id='search-tree'></ul>
         <aside id='scroll-gage'>0%</aside>
         <main>
         `),
-        contents_buffer,
-        Buffer.from('</main>'),
-    ])
+		contents_buffer,
+		Buffer.from('</main>'),
+	])
 }
 
 /* File System Manipulation */
 
-function readFile(path, options){
-    return new Promise((resolve,error)=>{
-        fs.readFile(path, options, function(err, buffer){
-            err && error()
-            resolve(buffer.toString())
-        })
-    })
+function readFile(path, options) {
+	return new Promise((resolve, error) => {
+		fs.readFile(path, options, function (err, buffer) {
+			err && error()
+			resolve(buffer.toString())
+		})
+	})
 }
 
-function readHierarchy(file_path, criteria_path){
-    const stats = fs.lstatSync(file_path)
+function readHierarchy(file_path, criteria_path) {
+	const stats = fs.lstatSync(file_path)
 
-    const file_name = path.basename(file_path, '.html')
-    let info = {
-        path: path.relative(criteria_path, file_path).replace(/..(?:\\|\/)/, ''),
-        name: file_name == 'dist' ? 'Open Archive' : file_name 
-    }
+	const file_name = path.basename(file_path, '.html')
+	let info = {
+		path: path.relative(criteria_path, file_path).replace(/..(?:\\|\/)/, ''),
+		name: file_name == 'dist' ? 'Open Archive' : file_name,
+	}
 
-    if (stats.isDirectory()) {
-        info.type = "folder"
-        info.children = fs.readdirSync(file_path, { withFileTypes:true })
-            .sort((file_alpha, file_beta)=>{
-                if(file_alpha.isDirectory() && file_beta.isDirectory()) return 0
-                if(file_alpha.isDirectory()) return -1
-                return 1
-            })
-            .map(file=>file.name)
-            .map(child_path=>readHierarchy(path.join(file_path, child_path), criteria_path))
-    } else {
-        info.type = "file"
-    }
+	if (stats.isDirectory()) {
+		info.type = 'folder'
+		info.children = fs
+			.readdirSync(file_path, { withFileTypes: true })
+			.sort((file_alpha, file_beta) => {
+				if (file_alpha.isDirectory() && file_beta.isDirectory()) return 0
+				if (file_alpha.isDirectory()) return -1
+				return 1
+			})
+			.map((file) => file.name)
+			.map((child_path) => readHierarchy(path.join(file_path, child_path), criteria_path))
+	} else {
+		info.type = 'file'
+	}
 
-    return info
+	return info
 }
 
+const server = new WebSocketServer({ port: 3212 })
+let web_socket
 
-const server = new WebSocketServer({ port: 3212 });
-let web_socket; 
-
-server.on("connection", function(webSocket) {
-    web_socket = webSocket
+server.on('connection', function (webSocket) {
+	web_socket = webSocket
 })
 
 /* Task */
- 
-gulp.task('scss', function(){
-    const src_fonts_css = gulp.src(path.join(Path.css, 'src_fonts.css'))
 
-    return gulp.src('./src/**/*.scss')
-        .pipe(scss().on('error', scss.logError))
-        .pipe(css_base64({
-            baseDir: './src/static',
-            debug: true
-        }).on('error', _=>_))
-        .pipe(src_fonts_css)
-        .pipe(concat('app.css'))
-        .pipe(gulp.dest(Path.temp))
-})  
+gulp.task('scss', function () {
+	const src_fonts_css = gulp.src(path.join(Path.css, 'src_fonts.css'))
 
-gulp.task('scss::build', function(){
-    const fonts_css = gulp.src(path.join(Path.css, 'fonts.css'))
-    
-    return gulp.src('./src/**/*.scss')
-        .pipe(scss().on('error', scss.logError))
-        .pipe(fonts_css)
-        .pipe(css_base64({
-            baseDir: './src/static',
-            debug: true
-        }).on('error', _=>_))
-        .pipe(concat('app.css'))
-        .pipe(minify())
-        .pipe(gulp.dest(Path.temp))
-})  
+	return gulp
+		.src('./src/**/*.scss')
+		.pipe(scss().on('error', scss.logError))
+		.pipe(
+			replace(/url\((?:"|')(.+)(?:"|')\)/g, function (full_text, url) {
+				if (url.startsWith('http')) return full_text
+				const static_file_path = path.join(Path.static, url).replace(/\\/g, '/')
+				return `url('${static_file_path}')`
+			})
+		)
+		.pipe(src_fonts_css)
+		.pipe(concat('app.css'))
+		.pipe(gulp.dest(Path.temp))
+})
 
+gulp.task('scss::build', function () {
+	const fonts_css = gulp.src(path.join(Path.css, 'fonts.css'))
+
+	return gulp
+		.src('./src/**/*.scss')
+		.pipe(scss().on('error', scss.logError))
+		.pipe(fonts_css)
+		.pipe(
+			css_base64({
+				baseDir: './src/static',
+				debug: false,
+			}).on('error', (_) => _)
+		)
+		.pipe(concat('app.css'))
+		.pipe(minify())
+		.pipe(gulp.dest(Path.temp))
+})
 
 /* Fonts */
 
-gulp.task('fonts', function() {
-    // create an accumulated stream
-    var fontStream = merge();
-  
-    fontStream.add(gulp.src(path.join(Path.css, 'fonts', 'maplestory', 'Maplestory_Light.ttf'))
-                    .pipe(inlineFonts({ name: 'Maplestory', weight: 400, formats: ['ttf'] })));
+gulp.task('fonts', function () {
+	// create an accumulated stream
+	var fontStream = merge()
 
-    // an italic version
-    fontStream.add(gulp.src(path.join(Path.css, 'fonts', 'maplestory', 'Maplestory_Bold.ttf'))
-                    .pipe(inlineFonts({ name: 'Maplestory', weight: 700, formats: ['ttf']  })));
+	fontStream.add(gulp.src(path.join(Path.css, 'fonts', 'maplestory', 'Maplestory_Light.ttf')).pipe(inlineFonts({ name: 'Maplestory', weight: 400, formats: ['ttf'] })))
 
+	// an italic version
+	fontStream.add(gulp.src(path.join(Path.css, 'fonts', 'maplestory', 'Maplestory_Bold.ttf')).pipe(inlineFonts({ name: 'Maplestory', weight: 700, formats: ['ttf'] })))
 
-    return fontStream.pipe(concat('fonts.css')).pipe(gulp.dest(Path.css));
-  });
+	return fontStream.pipe(concat('fonts.css')).pipe(gulp.dest(Path.css))
+})
 
+gulp.task('js', function () {
+	return code_syntax_js.pipe(gulp.src('./src/**/*.js')).pipe(concat('app.js')).pipe(minify()).pipe(gulp.dest(Path.temp))
+})
 
-
-gulp.task('js', function(){
-    return code_syntax_js
-        .pipe(gulp.src('./src/**/*.js'))
-        .pipe(concat('app.js'))
-        .pipe(minify()) 
-        .pipe(gulp.dest(Path.temp))
-}) 
-
-gulp.task('js::build', function(){
-    return code_syntax_js
-        .pipe(gulp.src([
-            './src/**/*.js',
-            //ignore
-            '!./src/**/hot_reload.js'
-        ]))
-        .pipe(concat('app.js'))
-        .pipe(minify()) 
-        .pipe(gulp.dest(Path.temp))
-}) 
-
- 
+gulp.task('js::build', function () {
+	return code_syntax_js
+		.pipe(gulp.src(['./src/**/*.js', '!./src/**/hot_reload.js']))
+		.pipe(concat('app.js'))
+		.pipe(minify())
+		.pipe(gulp.dest(Path.temp))
+})
 
 gulp.task('markdown', function () {
-    return gulp.src('./src/markdown/**/*.md')
-        .pipe(markdown())
-        .pipe(through.obj(async function (file, encoding, push){
-            const extra_css = toStyle(await readFile(path.join(Path.temp, 'app.css')))
-            const extra_js  = toScript(await readFile(path.join(Path.temp, 'app.js')))
-            
-            file.contents = Buffer.concat([
-                //header
-                Buffer.from(meta_tag_html),
-                Buffer.from(extra_css),
-                wrapContents(file.contents),
-                Buffer.from(extra_js),
-            ])
-            push(null, file)
-        }))
-        .pipe(html_base64('./src/static/img'))
-        .pipe(gulp.dest('./dist'))
-        .pipe(through.obj(async function(file, encoding, push){
-            const dist_html_path = file.history[file.history.length-1]
-            const fs_hierarchy_literal = JSON.stringify(readHierarchy(Path.dist, dist_html_path)).replace(/\\\\/g, '/')
-            const content = file.contents.toString()
-            file.contents = Buffer.from( content.replace('#tree_json_literal#', ()=>fs_hierarchy_literal), encoding)
-            
-            push(null, file)
-        }))
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('./dist'))
+	return gulp
+		.src('./src/markdown/**/*.md')
+		.pipe(markdown())
+		.pipe(
+			through.obj(async function (file, encoding, push) {
+				const extra_css = toStyle(await readFile(path.join(Path.temp, 'app.css')))
+				const extra_js = toScript(await readFile(path.join(Path.temp, 'app.js')))
+
+				file.contents = Buffer.concat([
+					//header
+					Buffer.from(meta_tag_html),
+					Buffer.from(extra_css),
+					wrapContents(file.contents),
+					Buffer.from(extra_js),
+				])
+				push(null, file)
+			})
+		)
+		.pipe(html_base64('./src/static/img'))
+		.pipe(gulp.dest('./dist'))
+		.pipe(
+			through.obj(async function (file, encoding, push) {
+				const dist_html_path = file.history[file.history.length - 1]
+				const fs_hierarchy_literal = JSON.stringify(readHierarchy(Path.dist, dist_html_path)).replace(/\\\\/g, '/')
+				const content = file.contents.toString()
+				file.contents = Buffer.from(
+					content.replace('#tree_json_literal#', () => fs_hierarchy_literal),
+					encoding
+				)
+
+				push(null, file)
+			})
+		)
+		.pipe(gulp.dest('./dist'))
 })
 
 gulp.task('markdown::build', function () {
-    return gulp.src('./src/markdown/**/*.md')
-        .pipe(markdown())
-        .pipe(through.obj(async function (file, encoding, push){
-            const extra_css = toStyle(await readFile(path.join(Path.temp, 'app.css')))
-            const extra_js  = toScript(await readFile(path.join(Path.temp, 'app-min.js')))
-            
-            file.contents = Buffer.concat([
-                //header
-                Buffer.from(meta_tag_html),
-                Buffer.from(extra_css),
-                wrapContents(file.contents),
-                Buffer.from(extra_js),
-            ])
-            push(null, file)
-        }))
-        .pipe(html_base64('./src/static/img'))
-        .pipe(gulp.dest('./dist'))
-        .pipe(through.obj(async function(file, encoding, push){
-            const dist_html_path = file.history[file.history.length-1]
-            const fs_hierarchy_literal = JSON.stringify(readHierarchy(Path.dist, dist_html_path)).replace(/\\\\/g, '/')
-            const content = file.contents.toString()
-            file.contents = Buffer.from( content.replace('#tree_json_literal#', ()=>fs_hierarchy_literal), encoding)
-            
-            push(null, file)
-        }))
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('./dist'))
+	return gulp
+		.src('./src/markdown/**/*.md')
+		.pipe(markdown())
+		.pipe(
+			through.obj(async function (file, encoding, push) {
+				const extra_css = toStyle(await readFile(path.join(Path.temp, 'app.css')))
+				const extra_js = toScript(await readFile(path.join(Path.temp, 'app.js')))
+
+				file.contents = Buffer.concat([
+					//header
+					Buffer.from(meta_tag_html),
+					Buffer.from(extra_css),
+					wrapContents(file.contents),
+					Buffer.from(extra_js),
+				])
+				push(null, file)
+			})
+		)
+		.pipe(html_base64('./src/static/img'))
+		.pipe(gulp.dest('./dist'))
+		.pipe(
+			through.obj(async function (file, encoding, push) {
+				const dist_html_path = file.history[file.history.length - 1]
+				const fs_hierarchy_literal = JSON.stringify(readHierarchy(Path.dist, dist_html_path)).replace(/\\\\/g, '/')
+				const content = file.contents.toString()
+				file.contents = Buffer.from(
+					content.replace('#tree_json_literal#', () => fs_hierarchy_literal),
+					encoding
+				)
+
+				push(null, file)
+			})
+		)
+		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(gulp.dest('./dist'))
 })
 
-
-gulp.task('reload', function(){ 
-    return new Promise(resolve=>{
-        setTimeout(_=>{
-            web_socket && web_socket.send('reload')
-            resolve()
-        }, 10)
-    })
+gulp.task('reload', function () {
+	return new Promise((resolve) => {
+		setTimeout((_) => {
+			web_socket && web_socket.send('reload')
+			resolve()
+		}, 10)
+	})
 })
 
 let watch_stream_list = []
- 
+
 gulp.task('watch', function () {
-    watch_stream_list = watch_stream_list.concat(
-          gulp.watch(['./src/**/*.md'], gulp.series(['markdown', 'reload']))
-        , gulp.watch(['./src/**/*.scss'], gulp.series(['scss', 'markdown', 'reload']))
-        , gulp.watch(['./src/**/*.js'], gulp.series(['js','markdown', 'reload']))
-    )
+	watch_stream_list = watch_stream_list.concat(gulp.watch(['./src/**/*.md'], gulp.series(['markdown', 'reload'])), gulp.watch(['./src/**/*.scss'], gulp.series(['scss', 'markdown', 'reload'])), gulp.watch(['./src/**/*.js'], gulp.series(['js', 'markdown', 'reload'])))
 })
 
-gulp.task('default', gulp.series(['scss','js','markdown', 'watch']))
+gulp.task('default', gulp.series(['scss', 'js', 'markdown', 'watch']))
 
-gulp.task('build', async function(){
-    await console.log(gulp.series(['scss::build','js::build','markdown::build'])())
-    server.close()
-    shell.rm('-r', Path.temp)
+gulp.task('build', function () {
+	return gulp.series(['scss::build', 'js::build', 'markdown::build'])()
+	// server.close()
+	// shell.rm('-r', Path.temp)
 })
 
-
-
-process.on('SIGINT', function() { 
-    server.close() 
-    shell.rm('-r', Path.temp)
-    watch_stream_list.forEach(watch_stream=>{
-        watch_stream.close()
-    })
+process.on('SIGINT', function () {
+	server.close()
+	shell.rm('-r', Path.temp)
+	watch_stream_list.forEach((watch_stream) => {
+		watch_stream.close()
+	})
 })
--- src/static/js/widget.js
+++ src/static/js/widget.js
@@ -2,39 +2,36 @@
 const scroll_gage_element = document.querySelector('#scroll-gage')
 const html_element = document.querySelector('html')
 
-window.addEventListener('scroll', function(){
-    const entrie_height = html_element.offsetHeight - window.innerHeight 
-    scroll_gage_element.innerText = `${Math.ceil(window.scrollY / entrie_height * 100) }%`
+window.addEventListener('scroll', function () {
+	const entrie_height = html_element.offsetHeight - window.innerHeight
+	scroll_gage_element.innerText = `${Math.ceil((window.scrollY / entrie_height) * 100)}%`
 })
-  
+
 const scroll_stack = []
-function consumeScrollStack(){
-    while(scroll_stack.length){
-        const distance = scroll_stack.shift()
-        scrollTo(0, window.scrollY + distance )
-    }
+function consumeScrollStack() {
+	while (scroll_stack.length) {
+		const distance = scroll_stack.shift()
+		scrollTo(0, window.scrollY + distance)
+	}
 }
 
-window.addEventListener('mousewheel', function(e){
-    if(!document.querySelector('#root').classList.contains('expanded')){
-        new Array(10).fill('').forEach(()=>{
-            scroll_stack.push(-e.wheelDeltaY/10)
-        })
-        consumeScrollStack()
-    }   
+window.addEventListener('mousewheel', function (e) {
+	if (!document.querySelector('#root').classList.contains('expanded')) {
+		new Array(10).fill('').forEach(() => {
+			scroll_stack.push(-e.wheelDeltaY / 10)
+		})
+		consumeScrollStack()
+	}
 })
 
-window.addEventListener('touchstart', function(e){
-    
-})
-
+window.addEventListener('touchstart', function (e) {})
 
 //앵커 설정
 let root_count = 0
-function castToHierarchyTree(tree){
-    root_count ++
-    if(tree.type == 'folder'){
-        return `
+function castToHierarchyTree(tree) {
+	root_count++
+	if (tree.type == 'folder') {
+		return `
             <li id="${root_count == 1 ? 'root' : ''}" class='${tree.type}' >
                 <label>${tree.name}</label>
                 <ul>
@@ -42,106 +39,106 @@ function castToHierarchyTree(tree){
                 </ul>
             </li>
         `
-    }else{
-        return `
+	} else {
+		return `
             <li class='${tree.type}'> 
                 <a href="${tree.path}">${tree.name}</a>
             </li>
-        ` 
-    }
+        `
+	}
 }
 
 //트리 아카이브 설정
 const tree_element = document.querySelector('#search-tree')
 tree_element.innerHTML = castToHierarchyTree(JSON.parse(`#tree_json_literal#`))
+;[document.getElementById('search-tree')].forEach((element) =>
+	element.addEventListener('mousewheel', function (e) {
+		const search_tree_element = document.getElementById('search-tree')
+		const marginTop = parseInt(String(search_tree_element.style.marginTop || 0).match(/-?\d+/)[0])
+		search_tree_element.style.marginTop = marginTop + e.wheelDeltaY / 5
+	})
+)
 
-document.querySelector('#search-tree>#root>label').addEventListener('click', async function(e){ 
-    const class_name = 'expanded'
-    const root_element = e.target.parentNode
-    
-    //pending
-    root_element.classList.toggle('pending')
-    if(root_element.classList.contains('pending'))await delay(300);
-    root_element.classList.toggle(class_name);
-    
-    (function expandElement(children){
-        if(children){
-            for(let child of children){
-                child.classList.toggle(class_name)
-                expandElement(child.children)
-            }
-        }
-    })([ e.target.parentNode.children[1] ])
+document.querySelector('#search-tree>#root>label').addEventListener('click', async function (e) {
+	const class_name = 'expanded'
+	const root_element = e.target.parentNode
 
-    //백그라운드 컬러 조정
-    document.querySelector('#background').classList.toggle(class_name);
-    
-    //main 태크 마진 맞추기
-    const main_element = document.querySelector('main')
-    const is_expanded = e.target.parentNode.classList.contains(class_name)
-    const default_margin_top = '7em'
-    main_element.style.marginTop = is_expanded ? `calc( ${window.scrollY}px + ${default_margin_top} + ${root_element.offsetHeight}px` : default_margin_top 
-    
-    const html_element = document.querySelector('html')
-    html_element.classList.toggle(class_name)
+	//pending
+	root_element.classList.toggle('pending')
+	if (root_element.classList.contains('pending')) await delay(300)
+	root_element.classList.toggle(class_name)
+	;(function expandElement(children) {
+		if (children) {
+			for (let child of children) {
+				child.classList.toggle(class_name)
+				expandElement(child.children)
+			}
+		}
+	})([e.target.parentNode.children[1]])
+
+	//백그라운드 컬러 조정
+	document.querySelector('#background').classList.toggle(class_name)
+
+	//main 태크 마진 맞추기
+	const main_element = document.querySelector('main')
+	const is_expanded = e.target.parentNode.classList.contains(class_name)
+	const default_margin_top = '7em'
+	main_element.style.marginTop = is_expanded ? `calc( ${window.scrollY}px + ${default_margin_top} + ${root_element.offsetHeight}px` : default_margin_top
+
+	const html_element = document.querySelector('html')
+	html_element.classList.toggle(class_name)
 })
 // document.querySelector('#search-tree>#root>label').click()
 
-    //라벨 설정
-const label_element_list = [ ...document.querySelectorAll('#search-tree>#root li.folder>label') ]
-label_element_list.forEach(label_element=>{
-    //클릭 이벤트
-    label_element.addEventListener('click', function(e){
-        const unordered_element_list = [ ...e.target.parentNode.children ]    
-        unordered_element_list.shift()
-    
-        for(const unordered_element of unordered_element_list){
-            const class_name = 'expanded'
-            unordered_element.classList.toggle(class_name)
-            
-            const list_element_list = unordered_element.children
-            for(const list_element of list_element_list){
-                list_element.classList.toggle(class_name)
-                const anchor_element_list = list_element.children
-                for(const anchor_element of anchor_element_list){
-                    if(anchor_element.tagName == 'A')anchor_element.classList.toggle(class_name)
-                }
-            }    
-        }
+//라벨 설정
+const label_element_list = [...document.querySelectorAll('#search-tree>#root li.folder>label')]
+label_element_list.forEach((label_element) => {
+	//클릭 이벤트
+	label_element.addEventListener('click', function (e) {
+		const unordered_element_list = [...e.target.parentNode.children]
+		unordered_element_list.shift()
 
-        //main 태크 마진 맞추기
-        const root_element = document.querySelector('#root')
-        const main_element = document.querySelector('main')
-        const default_margin_top = '7em'
-        main_element.style.marginTop = `calc( ${window.scrollY}px + ${default_margin_top} + ${root_element.offsetHeight}px` 
-        
-    })
+		for (const unordered_element of unordered_element_list) {
+			const class_name = 'expanded'
+			unordered_element.classList.toggle(class_name)
 
+			const list_element_list = unordered_element.children
+			for (const list_element of list_element_list) {
+				list_element.classList.toggle(class_name)
+				const anchor_element_list = list_element.children
+				for (const anchor_element of anchor_element_list) {
+					if (anchor_element.tagName == 'A') anchor_element.classList.toggle(class_name)
+				}
+			}
+		}
+
+		//main 태크 마진 맞추기
+		const root_element = document.querySelector('#root')
+		const main_element = document.querySelector('main')
+		const default_margin_top = '7em'
+		main_element.style.marginTop = `calc( ${window.scrollY}px + ${default_margin_top} + ${root_element.offsetHeight}px`
+	})
 })
 
-    //드래그방지
-const tree_element_list = [ 
-    ...document.querySelectorAll('#search-tree, #search-tree *'),
-    document.querySelector('#background')
-]
-tree_element_list.forEach(tree_element=>{
-    //드래그 방지
-    tree_element.addEventListener('mousedown', function(event) {
-        console.log('start')
-        event.preventDefault();
-        mouseDown = true;
-    })
-    tree_element.addEventListener('touchstart', function(event) {
-        console.log('start')
-        event.preventDefault();
-        mouseDown = true;
-    })
+//드래그방지
+const tree_element_list = [...document.querySelectorAll('#search-tree, #search-tree *'), document.querySelector('#background')]
+tree_element_list.forEach((tree_element) => {
+	//드래그 방지
+	tree_element.addEventListener('mousedown', function (event) {
+		console.log('start')
+		event.preventDefault()
+		mouseDown = true
+	})
+	tree_element.addEventListener('touchstart', function (event) {
+		console.log('start')
+		event.preventDefault()
+		mouseDown = true
+	})
 })
-
 
 //async util
-function delay(ms){
-    return new Promise(resolve=>{
-        setTimeout(resolve, ms)
-    })
+function delay(ms) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms)
+	})
 }
